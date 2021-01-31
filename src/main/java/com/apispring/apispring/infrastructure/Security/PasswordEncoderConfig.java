15203f52b5bb276166dc3b9b8d44d79b8a23e059
Alejandro Aranda <alexarnd99@gmail.com>
2021-01-31 23:22:50 +0100

    Create pass encoder config file

---
+++ src/main/java/com/apispring/apispring/infrastructure/Security/PasswordEncoderConfig.java
@@ -0,0 +1,15 @@
+package com.apispring.apispring.infrastructure.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new Argon2PasswordEncoder();
+    }
+}
