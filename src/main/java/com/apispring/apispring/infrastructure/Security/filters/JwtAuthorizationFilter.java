7a75206ea45b368a64a5a956dffc7f564aaba4cb
Alejandro Aranda <alexarnd99@gmail.com>
2021-02-03 15:38:54 +0100

    Finish doInternalFilter method

--- src/main/java/com/apispring/apispring/infrastructure/Security/filters/JwtAuthorizationFilter.java
+++ src/main/java/com/apispring/apispring/infrastructure/Security/filters/JwtAuthorizationFilter.java
@@ -1,8 +1,15 @@
 package com.apispring.apispring.infrastructure.Security.filters;
 
 import com.apispring.apispring.application.services.ServiceSecurity;
+import com.apispring.apispring.domain.model.UserModel;
 import com.apispring.apispring.infrastructure.Security.service.JwtService;
+import com.apispring.apispring.infrastructure.entity.User;
+import com.apispring.apispring.infrastructure.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -19,6 +26,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final ServiceSecurity serviceSecurity;
+    private final UserMapper userMapper;
 
     @Override
     protected void doFilterInternal(
@@ -28,7 +36,15 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         String token = getJwtFromRequest(httpServletRequest);
 
-        if (token != null && jwtService.validateToken(token)){
+        if (token != null && this.jwtService.validateToken(token)){
+            int userId = this.jwtService.getIdOfUserFromJwt(token);
+
+            User user = this.userMapper.toEntity(this.serviceSecurity.loadUserById(userId));
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user, user.getRoles(), user.getAuthorities());
+            authenticationToken.setDetails(new WebAuthenticationDetails(httpServletRequest));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
         }
 
@@ -36,11 +52,11 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     }
 
     private String getJwtFromRequest(HttpServletRequest httpServletRequest) {
-        String bearerToken = httpServletRequest.getHeader(JwtService.TOKEN_HEADER);
+        String bearerToken = httpServletRequest.getHeader(this.jwtService.TOKEN_HEADER);
         String tokenWithoutBearer = null;
 
-        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(JwtService.TOKEN_PREFIX)){
-            tokenWithoutBearer = bearerToken.substring(JwtService.TOKEN_PREFIX.length(), bearerToken.length());
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(this.jwtService.TOKEN_PREFIX)){
+            tokenWithoutBearer = bearerToken.substring(this.jwtService.TOKEN_PREFIX.length(), bearerToken.length());
         }
         return tokenWithoutBearer;
     }
